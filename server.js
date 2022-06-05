a1b6124940ad3ef866c092a625d85c5f2b9ed9df
Amit Akuka <amit.akoka98@gmail.com>
2022-06-05 23:34:19 +0300

    production

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cookieParser())
 app.use(express.json({ limit: '50mb' }))
 // app.use(express.bodyParser({ limit: '50mb' }))
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, 'public')))
+  app.use(express.static(path.resolve(__dirname, 'public/build')))
 } else {
   const corsOptions = {
     origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
